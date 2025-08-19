f7bded9757d04e8d6d8e59a729a38af787e32f62
Markson Hon <markson@localhost.local>
2025-08-19 12:18:02 +0800

    add busybox user deletion

--- uninstaller.sh
+++ uninstaller.sh
@@ -5,6 +5,6 @@
 [ -f /usr/local/bin/headscale ] && rm -rf /usr/local/bin/headscale
 [ -f /etc/systemd/system/headscale.service ] && rm -rf /etc/systemd/system/headscale.service
 [ -f /etc/init.d/headscale ] && rm -rf /etc/init.d/headscale
-userdel headscale || deluser headscale
-groupdel headscale || delgroup headscale
+userdel headscale || busybox deluser headscale
+groupdel headscale || busybox delgroup headscale
 echo "headscale has been uninstalled, remove /etc/headscale manually if you want to."
